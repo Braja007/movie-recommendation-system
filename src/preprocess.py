3e2856a5c7d2e15af70292a1d4dee3c7fb168e00
Braja007 <brozodeka1844@gmail.com>
2025-06-19 20:46:48 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,30 +1,22 @@
+# src/preprocess.py
 import re
 import nltk
 import os
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-# Create a local nltk_data folder
+# Use local folder for NLTK data
 nltk_data_path = os.path.join(os.path.dirname(__file__), "nltk_data")
 os.makedirs(nltk_data_path, exist_ok=True)
-
-# Set the NLTK data path
 nltk.data.path.append(nltk_data_path)
 
-# Download required NLTK resources to the local folder
-nltk.download('punkt', download_dir=nltk_data_path)
-nltk.download('stopwords', download_dir=nltk_data_path)
+# Download necessary resources
+nltk.download("punkt", download_dir=nltk_data_path)
+nltk.download("stopwords", download_dir=nltk_data_path)
 
-# Set of English stopwords
 stop_words = set(stopwords.words('english'))
 
 def preprocess_text(text):
-    """
-    Cleans and tokenizes input text.
-    - Removes non-alphabet characters
-    - Converts to lowercase
-    - Removes stopwords
-    """
     text = re.sub(r"[^a-zA-Z\s]", "", str(text))
     text = text.lower()
     tokens = word_tokenize(text)
