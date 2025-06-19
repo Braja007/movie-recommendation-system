8ab92ed2eb31b4304d2d93347114b5516da93165
Braja007 <brozodeka1844@gmail.com>
2025-06-19 20:52:03 +0530

    Update preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,24 +1,22 @@
-# src/preprocess.py
 import re
-import nltk
-import os
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
 
-# Use local folder for NLTK data
-nltk_data_path = os.path.join(os.path.dirname(__file__), "nltk_data")
-os.makedirs(nltk_data_path, exist_ok=True)
-nltk.data.path.append(nltk_data_path)
-
-# Download necessary resources
-nltk.download("punkt", download_dir=nltk_data_path)
-nltk.download("stopwords", download_dir=nltk_data_path)
-
-stop_words = set(stopwords.words('english'))
+# Basic stopword list (manually added)
+stop_words = {
+    "a", "an", "the", "is", "in", "on", "and", "or", "if", "of", "to", "with", "as",
+    "for", "was", "were", "by", "this", "that", "it", "at", "from", "but", "be", "are",
+    "have", "has", "had", "not", "can", "will", "just", "they", "them", "he", "she",
+    "you", "we", "i", "do", "does", "did", "so", "than", "too"
+}
 
 def preprocess_text(text):
-    text = re.sub(r"[^a-zA-Z\s]", "", str(text))
+    """
+    Cleans and tokenizes input text:
+    - Removes non-alphabet characters
+    - Converts to lowercase
+    - Removes stopwords
+    """
+    text = re.sub(r"[^a-zA-Z\s]", "", str(text))  # Remove non-alphabetic
     text = text.lower()
-    tokens = word_tokenize(text)
+    tokens = text.split()  # Simple whitespace tokenization
     tokens = [word for word in tokens if word not in stop_words]
     return " ".join(tokens)
